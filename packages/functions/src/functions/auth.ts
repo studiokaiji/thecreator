f7faba2a62ec19fae6cdd9357af2e67f2e76b291
Haruki <nazawaharuki@gmail.com>
2022-10-25 08:53:38 +0900

    fixed cors error

--- packages/functions/src/functions/auth.ts
+++ packages/functions/src/functions/auth.ts
@@ -6,9 +6,7 @@ import * as firebaseAdmin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 const admin = firebaseAdmin.initializeApp();
-const cors = corsLib({
-  origin: true,
-});
+const cors = corsLib();
 
 export const getNonceToSign = functions.https.onRequest((request, response) =>
   cors(request, response, async () => {
