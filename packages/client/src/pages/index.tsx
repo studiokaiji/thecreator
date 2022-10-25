47d7b5aec846e3f23ea5d9804f3f571f4b4c62f3
Haruki <nazawaharuki@gmail.com>
2022-10-25 09:24:06 +0900

    implements metamask auth

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,16 +1,20 @@
-import { useWallet } from '../hooks/useWallet';
+import { useAuth } from '@/hooks/useAuth';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { useWallet } from '@/hooks/useWallet';
 
 export const IndexPage = () => {
-  const { account, activate, active, chainId, deactivate } = useWallet();
+  const { account, active, chainId } = useWallet();
 
-  
+  const { signIn, signOut } = useAuth();
+  const { currentUser } = useCurrentUser();
 
   return (
     <div>
       <div>Connection Status: {active.toString()}</div>
       <p>Account: {account?.toString()}</p>
       <div>Network ID: {chainId?.toString()}</div>
-      <button onClick={() => (active ? deactivate() : activate('injected'))}>
+      <div>CurrentUser: {JSON.stringify(currentUser, null, 2)}</div>
+      <button onClick={() => (active ? signIn('injected') : signOut())}>
         {active ? 'Disconnect Wallet' : 'Connect Wallet'}
       </button>
     </div>
