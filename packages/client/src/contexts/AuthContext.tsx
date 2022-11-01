d19e0b8fb6d570625a86d81214f832fdf13d2739
Haruki <nazawaharuki@gmail.com>
2022-11-01 16:46:41 +0900

    add isConnectedWallet

--- packages/client/src/contexts/AuthContext.tsx
+++ packages/client/src/contexts/AuthContext.tsx
@@ -1,7 +1,9 @@
-import type { User } from 'firebase/auth';
+import { User } from 'firebase/auth';
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
 import { auth } from '@/firebase';
+import { useWallet } from '@/hooks/useWallet';
+import { isConnectedWallet } from '@/utils/is-connected-wallet';
 
 type AuthContextProps = {
   currentUser: User | null | undefined;
@@ -18,13 +20,21 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
     undefined
   );
 
+  const { account } = useWallet();
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(setCurrentUser);
     return () => unsubscribe();
   }, []);
 
+  useEffect(() => {
+    isConnectedWallet(currentUser?.uid).then(async (isConnected) => {
+      if (!isConnected && currentUser) await auth.signOut();
+    });
+  }, [account]);
+
   return (
-    <AuthContext.Provider value={{ currentUser: currentUser }}>
+    <AuthContext.Provider value={{ currentUser }}>
       {children}
     </AuthContext.Provider>
   );
