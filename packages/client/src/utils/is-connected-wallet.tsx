476bc2b917d5800d24b731cd4e94af202d16eb5f
Haruki <nazawaharuki@gmail.com>
2022-11-05 20:43:06 +0900

    remove console.log

--- packages/client/src/utils/is-connected-wallet.tsx
+++ packages/client/src/utils/is-connected-wallet.tsx
@@ -5,8 +5,6 @@ export const isConnectedWallet = async (
   address = address.toLocaleLowerCase();
   const accounts = await ethereum.request({ method: 'eth_accounts' });
 
-  console.log(address, accounts);
-
   if (accounts.length === 0) {
     return false;
   }
