f3160e1d35757c83bbe20a6acc5a8e0fd2d908f2
Haruki <nazawaharuki@gmail.com>
2022-11-06 21:43:38 +0900

    Create logic part

--- packages/client/src/pages/create.tsx
+++ packages/client/src/pages/create.tsx
@@ -1,3 +1,137 @@
+import { TheCreatorProductFactory__factory } from '@contracts/index';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { setDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+import { getCreatorDocRef } from '@/converters/creators';
+import { useWallet } from '@/hooks/useWallet';
+
+type ReceiveToken = 'weth' | 'usdc';
+
+type CreatePageInputs = {
+  creatorName: string;
+  receiveToken: ReceiveToken;
+};
+
+const tokenAddresses: { [token in ReceiveToken]: string } = {
+  usdc: import.meta.env.VITE_USDC_ADDRESS,
+  weth: import.meta.env.VITE_WETH_ADDRESS,
+};
+
 export const CreatePage = () => {
-  return <div>A</div>;
+  const { getValues, register } = useForm<CreatePageInputs>();
+
+  const [txHash, setTxHash] = useState('');
+  const [status, setStatus] = useState<
+    'typing' | 'waitingSendTx' | 'deploying' | 'deployed' | 'failedToSendTx'
+  >('typing');
+  const [contractAddress, setContractAddress] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { account, library } = useWallet();
+
+  const onClickCreatePageButtonHandler = () => {
+    if (!library || !account) return;
+
+    (async () => {
+      setStatus('waitingSendTx');
+
+      const factoryContract = TheCreatorProductFactory__factory.connect(
+        import.meta.env.VITE_PRODUCT_FACTORY_CONTRACT_ADDRESS,
+        library.getSigner()
+      );
+
+      const { creatorName, receiveToken } = getValues();
+
+      const txRes = await factoryContract.create(
+        creatorName,
+        'SYMBOL',
+        tokenAddresses[receiveToken]
+      );
+      setStatus('deploying');
+      setTxHash(txRes.hash);
+
+      const docRef = getCreatorDocRef(account);
+      await setDoc(docRef, {
+        contractAddress: '',
+        creatorName,
+        description: '',
+        pinningPostId: '',
+        txHash,
+      });
+
+      const txReceipt = await txRes.wait();
+      setStatus('deployed');
+
+      const event = txReceipt.events?.filter(
+        ({ event }) => event === 'Deployed'
+      )[0];
+      if (!event || !event.args) throw Error('Invalid event');
+
+      const contractAddress = event.args.product;
+      setContractAddress(contractAddress);
+
+      await setDoc(docRef, {
+        contractAddress,
+        txHash: '',
+      });
+    })().catch((e) => {
+      setStatus('failedToSendTx');
+      console.log(e);
+      setErrorMessage(JSON.stringify(e, null, 2));
+    });
+  };
+
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ m: 'auto' }}>
+      <Typography align="center" variant="h3">
+        {t('becomeACreator')}
+      </Typography>
+      {status === 'typing' ? (
+        <form>
+          <TextField
+            {...register('creatorName')}
+            label={t('creatorName')}
+            sx={{ width: '100%' }}
+            variant="standard"
+          />
+          <FormControl sx={{ width: '100%' }} variant="standard">
+            <InputLabel>{t('receiveToken')}</InputLabel>
+            <Select {...register('receiveToken')} defaultValue="usdc">
+              <MenuItem value="weth">ETH(WETH)</MenuItem>
+              <MenuItem value="usdc">USDC</MenuItem>
+            </Select>
+          </FormControl>
+          <Button onClick={onClickCreatePageButtonHandler} variant="contained">
+            {t('create')}
+          </Button>
+        </form>
+      ) : (
+        <Box sx={{ textAlign: 'center' }}>
+          <Typography variant="h6">{t(status)}</Typography>
+          {txHash && <Typography>Transaction Hash: {txHash}</Typography>}
+          {contractAddress && (
+            <Button
+              onClick={onClickCreatePageButtonHandler}
+              variant="contained"
+            >
+              {t('goToCreatorPage')}
+            </Button>
+          )}
+          {errorMessage && <Typography color="red">{errorMessage}</Typography>}
+        </Box>
+      )}
+    </Box>
+  );
 };
