7f13dae5ceba930ac1aa6f1c95ea89ff29a82598
Haruki <nazawaharuki@gmail.com>
2022-11-07 19:42:09 +0900

    add swr and create useCreator

---
+++ packages/client/src/hooks/useCreator.ts
@@ -0,0 +1,25 @@
+import { getDoc } from 'firebase/firestore';
+import useSWR from 'swr';
+
+import { useCurrentUser } from './useCurrentUser';
+
+import { getCreatorDocRef } from '@/converters/creators';
+
+export const useCreator = (address?: string) => {
+  if (!address) {
+    const { currentUser } = useCurrentUser();
+    if (currentUser) {
+      address = currentUser.uid;
+    } else if (currentUser === null) {
+      throw Error();
+    }
+  }
+
+  const fetcher = async (addr: string) => {
+    const ref = getCreatorDocRef(addr);
+    const snapshot = await getDoc(ref);
+    return snapshot.data();
+  };
+
+  return useSWR(() => address, fetcher);
+};
