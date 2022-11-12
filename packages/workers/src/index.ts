4ce3b5b95f71e8808e9f65d77e1bcd06660c5a2f
Haruki <nazawaharuki@gmail.com>
2022-11-13 02:28:33 +0900

    firebase-auth-cloudflare-workers => @honojs/firebase-auth

--- packages/workers/src/index.ts
+++ packages/workers/src/index.ts
@@ -1,7 +1,37 @@
+import {
+  getFirebaseToken,
+  verifyFirebaseAuth,
+  VerifyFirebaseAuthEnv,
+} from '@honojs/firebase-auth';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-const app = new Hono();
+const config = {
+  projectId: 'melt-thecreator',
+};
 
-app.get('/', (c) => {});
+const app = new Hono<{ Bindings: VerifyFirebaseAuthEnv }>();
+
+app.use(
+  '*',
+  cors({
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    credentials: true,
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    origin: 'http://localhost:5173',
+  }),
+  verifyFirebaseAuth(config)
+);
+
+app.get('/hello', (c) => {
+  return c.json('Hello');
+});
+
+app.get('/echo-id-token', (c) => {
+  const idToken = getFirebaseToken(c);
+  return c.json(idToken);
+});
 
 export default app;
