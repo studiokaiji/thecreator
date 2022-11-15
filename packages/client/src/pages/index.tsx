e2be15c6cebb1c2f7d16303754d2a85b0b998d05
Haruki <nazawaharuki@gmail.com>
2022-11-15 16:11:36 +0900

    add checkSubscription

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+
 export const IndexPage = () => {
-  return <div></div>;
+  const { currentUser } = useCurrentUser();
+
+  const [res, setRes] = useState('');
+
+  const onClickEchoIdTokenButtonHandler = async () => {
+    if (!currentUser) return;
+    const idToken = await currentUser.getIdToken();
+    fetch('http://127.0.0.1:8787/echo-id-token', {
+      headers: {
+        Authorization: idToken,
+      },
+      method: 'GET',
+    })
+      .then(async (r) => {
+        if (!r.ok) alert(r.status);
+        const data = await r.json();
+        setRes(JSON.stringify(data, null, 2));
+      })
+      .catch((e) => alert(String(e)));
+  };
+
+  return (
+    <div>
+      <pre>Res: {res}</pre>
+      <button onClick={onClickEchoIdTokenButtonHandler}>ECHO ID TOKEN</button>
+    </div>
+  );
 };
