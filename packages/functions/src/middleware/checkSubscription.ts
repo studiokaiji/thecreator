af6dd5e46a68ba3db52cf23408fd0875fdb7c478
Haruki <nazawaharuki@gmail.com>
2022-11-16 21:15:10 +0900

    Checkable subscription status with planId

--- packages/functions/src/middleware/checkSubscription.ts
+++ packages/functions/src/middleware/checkSubscription.ts
@@ -14,10 +14,22 @@ export type Subscription = {
   isValid: boolean;
 };
 
-const creatorAbi =
-  '[{"inputs":[{"internalType":"address","name":"_customer","type":"address"}],"name":"subscriptions","outputs":[{"components":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"lastMintedAt","type":"uint256"},{"internalType":"uint256","name":"planId","type":"uint256"},{"internalType":"uint256","name":"usage","type":"uint256"},{"internalType":"address","name":"nft","type":"address"},{"internalType":"string","name":"meta","type":"string"},{"internalType":"bool","name":"isValid","type":"bool"}],"internalType":"struct Product.Subscription","name":"_subsctiption","type":"tuple"}],"stateMutability":"view","type":"function"}]';
+export type Plan = {
+  usage: BigNumber;
+  nft: string;
+  meta: string;
+};
 
-const endpoints = ['127.0.0.1:8545'];
+const creatorAbi =
+  '[{"inputs":[{"internalType":"address","name":"_customer","type":"address"}],"name":"subscriptions","outputs":[{"components":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"lastMintedAt","type":"uint256"},{"internalType":"uint256","name":"planId","type":"uint256"},{"internalType":"uint256","name":"usage","type":"uint256"},{"internalType":"address","name":"nft","type":"address"},{"internalType":"string","name":"meta","type":"string"},{"internalType":"bool","name":"isValid","type":"bool"}],"internalType":"struct Product.Subscription","name":"_subsctiption","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_planId","type":"uint256"}],"name":"plans","outputs":[{"components":[{"internalType":"uint256","name":"usage","type":"uint256"},{"internalType":"address","name":"nft","type":"address"},{"internalType":"string","name":"meta","type":"string"}],"internalType":"struct Product.Plan","name":"_plan","type":"tuple"}],"stateMutability":"view","type":"function"}]';
+
+const multicallAbi =
+  '[{"constant":true,"inputs":[],"name":"getCurrentBlockTimestamp","outputs":[{"name":"timestamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"components":[{"name":"target","type":"address"},{"name":"callData","type":"bytes"}],"name":"calls","type":"tuple[]"}],"name":"aggregate","outputs":[{"name":"blockNumber","type":"uint256"},{"name":"returnData","type":"bytes[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getLastBlockHash","outputs":[{"name":"blockHash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"getEthBalance","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockDifficulty","outputs":[{"name":"difficulty","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockGasLimit","outputs":[{"name":"gaslimit","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockCoinbase","outputs":[{"name":"coinbase","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"blockNumber","type":"uint256"}],"name":"getBlockHash","outputs":[{"name":"blockHash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}]';
+
+const endpoints = JSON.parse(
+  process.env.CHAIN_RPC_ENDPOINTS as string
+) as string[];
+
 const provider = new FallbackProvider(
   endpoints.map((url) => new JsonRpcProvider(url)),
   1
@@ -26,11 +38,33 @@ const provider = new FallbackProvider(
 export const checkSubscription = async (
   id: string,
   creatorContractAddress: string,
-  usage: BigNumberish
+  planId: BigNumberish
 ) => {
-  // Getting Subscription from Creator Contracts
-  const contract = new Contract(creatorContractAddress, creatorAbi, provider);
-  const subscription: Subscription = await contract.subscriptions(id);
+  // Getting Subscription and plan from Creator contract with multicall
+  const contract = new Contract(creatorContractAddress, creatorAbi);
+  const multicallInputs = [
+    {
+      callData: contract.interface.encodeFunctionData('subscriptions', [id]),
+      target: creatorContractAddress,
+    },
+    {
+      callData: contract.interface.encodeFunctionData('plans', [planId]),
+      target: creatorContractAddress,
+    },
+  ];
+
+  const multicall = new Contract(
+    process.env.MULTICALL_ADDRESS as string,
+    multicallAbi,
+    provider
+  );
+  const { returnData } = await multicall.aggregate(multicallInputs);
+
+  const subscription: Subscription = returnData[0];
+  const plan: Plan = returnData[1];
+
   // Check if the plan you are signing up for meets your plan requirements.
-  return subscription.isValid && BigNumber.from(usage).gte(subscription.usage);
+  return (
+    subscription.isValid && BigNumber.from(plan.usage).gte(subscription.usage)
+  );
 };
