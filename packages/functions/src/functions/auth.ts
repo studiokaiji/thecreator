7d82fc2080af19d98ce290c82c80ff2a756d6a56
Haruki <nazawaharuki@gmail.com>
2022-11-18 02:32:02 +0900

    add instances

--- packages/functions/src/functions/auth.ts
+++ packages/functions/src/functions/auth.ts
@@ -2,15 +2,15 @@ import { randomUUID } from 'crypto';
 
 import { recoverPersonalSignature } from '@metamask/eth-sig-util';
 import * as corsLib from 'cors';
-import * as firebaseAdmin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
 
-const admin = firebaseAdmin.initializeApp();
+import { auth, db } from '../instances';
+
 const cors = corsLib();
 
 const NONCE_MESSAGE = 'Please sign for TheCreator account authentication.';
 
-export const getNonceToSign = functions.https.onRequest((request, response) =>
+export const getNonceToSign = https.onRequest((request, response) =>
   cors(request, response, async () => {
     try {
       if (request.method !== 'POST') {
@@ -20,8 +20,7 @@ export const getNonceToSign = functions.https.onRequest((request, response) =>
         return response.sendStatus(400);
       }
       // Get the user document for that address
-      const userDoc: FirebaseFirestore.DocumentSnapshot = await admin
-        .firestore()
+      const userDoc: FirebaseFirestore.DocumentSnapshot = await db
         .collection('users')
         .doc(request.body.address)
         .get();
@@ -36,11 +35,11 @@ export const getNonceToSign = functions.https.onRequest((request, response) =>
         const nonce = `${NONCE_MESSAGE}\n${generatedNonce}`;
 
         // Create an Auth user
-        const createdUser = await admin.auth().createUser({
+        const createdUser = await auth.createUser({
           uid: request.body.address,
         });
         // Associate the nonce with that user
-        await admin.firestore().collection('users').doc(createdUser.uid).set({
+        await db.collection('users').doc(createdUser.uid).set({
           nonce,
         });
         return response.status(200).json({ nonce: generatedNonce });
@@ -52,54 +51,53 @@ export const getNonceToSign = functions.https.onRequest((request, response) =>
   })
 );
 
-export const verifySignedMessage = functions.https.onRequest(
-  (request, response) =>
-    cors(request, response, async () => {
-      try {
-        if (request.method !== 'POST') {
-          return response.sendStatus(403);
-        }
-        if (!request.body.address || !request.body.signature) {
-          return response.sendStatus(400);
-        }
-        const address = request.body.address;
-        const sig = request.body.signature;
-        // Get the nonce for this address
-        const userDocRef = admin.firestore().collection('users').doc(address);
-        const userDoc = await userDocRef.get();
-        if (userDoc.exists) {
-          const existingNonce = userDoc.data()?.nonce;
-          // Recover the address of the account
-          // used to create the given Ethereum signature.
-          const recoveredAddress = recoverPersonalSignature({
-            data: `0x${toHex(existingNonce)}`,
-            signature: sig,
+export const verifySignedMessage = https.onRequest((request, response) =>
+  cors(request, response, async () => {
+    try {
+      if (request.method !== 'POST') {
+        return response.sendStatus(403);
+      }
+      if (!request.body.address || !request.body.signature) {
+        return response.sendStatus(400);
+      }
+      const address = request.body.address;
+      const sig = request.body.signature;
+      // Get the nonce for this address
+      const userDocRef = db.collection('users').doc(address);
+      const userDoc = await userDocRef.get();
+      if (userDoc.exists) {
+        const existingNonce = userDoc.data()?.nonce;
+        // Recover the address of the account
+        // used to create the given Ethereum signature.
+        const recoveredAddress = recoverPersonalSignature({
+          data: `0x${toHex(existingNonce)}`,
+          signature: sig,
+        });
+        // See if that matches the address
+        // the user is claiming the signature is from
+        if (recoveredAddress === address) {
+          // The signature was verified - update the nonce to prevent replay attacks
+          // update nonce
+          await userDocRef.update({
+            nonce: randomUUID(),
           });
-          // See if that matches the address
-          // the user is claiming the signature is from
-          if (recoveredAddress === address) {
-            // The signature was verified - update the nonce to prevent replay attacks
-            // update nonce
-            await userDocRef.update({
-              nonce: randomUUID(),
-            });
-            // Create a custom token for the specified address
-            const firebaseToken = await admin.auth().createCustomToken(address);
-            // Return the token
-            return response.status(200).json({ token: firebaseToken });
-          } else {
-            // The signature could not be verified
-            return response.sendStatus(401);
-          }
+          // Create a custom token for the specified address
+          const firebaseToken = auth.createCustomToken(address);
+          // Return the token
+          return response.status(200).json({ token: firebaseToken });
         } else {
-          console.log('user doc does not exist');
-          return response.sendStatus(500);
+          // The signature could not be verified
+          return response.sendStatus(401);
         }
-      } catch (err) {
-        console.log(err);
+      } else {
+        console.log('user doc does not exist');
         return response.sendStatus(500);
       }
-    })
+    } catch (err) {
+      console.log(err);
+      return response.sendStatus(500);
+    }
+  })
 );
 
 const toHex = (stringToConvert: string) =>
---
+++ packages/functions/src/instances.ts
@@ -0,0 +1,19 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { initializeApp } from 'firebase-admin';
+
+export const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.R2_ACCOUNT_ID || '',
+    secretAccessKey: 'SECRET_ACCESS_KEY',
+  },
+  endpoint: `https://${
+    process.env.R2_ACCOUNT_ID || ''
+  }.r2.cloudflarestorage.com`,
+  region: 'auto',
+});
+
+export const admin = initializeApp();
+
+export const db = admin.firestore();
+export const rtdb = admin.database();
+export const auth = admin.auth();
