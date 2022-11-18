5ee72c9750197e62cbf846eff5b490f5d6632980
Haruki <nazawaharuki@gmail.com>
2022-11-19 07:55:25 +0900

    add postConverter

---
+++ packages/functions/src/converters/postConverter.ts
@@ -0,0 +1,37 @@
+import { FirestoreDataConverter, Timestamp } from 'firebase-admin/firestore';
+
+const validContentsTypes = ['audio', 'text', 'images'];
+
+export type PostDocument = {
+  contentsType: 'audio' | 'text' | 'images';
+  contentsCount: number;
+  planId: string;
+  title: string;
+  description: string;
+  createdAt: Date;
+  lastChangedAt: Date;
+};
+
+export const postConverter: FirestoreDataConverter<PostDocument> = {
+  fromFirestore: (snapshot) => {
+    const data = snapshot.data();
+
+    const filtered = validContentsTypes.filter((c) => c === data.contentsType);
+    if (filtered.length < 1) {
+      throw Error('Invalid contentsType');
+    }
+
+    return {
+      contentsCount: data.contentsCount,
+      contentsType: data.contentsType,
+      createdAt: (data.createdAt as Timestamp).toDate(),
+      description: data.description,
+      lastChangedAt: (data.createdAt as Timestamp).toDate(),
+      planId: data.planId,
+      title: data.title,
+    };
+  },
+  toFirestore: (data) => {
+    return data;
+  },
+};
