11cccc37857b0199ad5d02e8a7b0790b368e53e4
Haruki <nazawaharuki@gmail.com>
2022-11-20 22:11:06 +0900

    createdAt => updatedAt

--- packages/client/@types/firestore/creator/index.d.ts
+++ packages/client/@types/firestore/creator/index.d.ts
@@ -1,9 +1,7 @@
-import { Timestamp } from 'firebase/firestore';
-
 export type CreatorDocData = {
   creatorAddress: string;
   creatorName: string;
   description: string;
   pinningPostId: string;
-  createdAt: Timestamp;
+  updatedAt: Date;
 };
