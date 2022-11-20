b61d53fde1b2c3b29f38340747a8ea8f444b5195
Haruki <nazawaharuki@gmail.com>
2022-11-20 22:11:37 +0900

    remove partial

--- packages/client/src/converters/creators.ts
+++ packages/client/src/converters/creators.ts
@@ -5,35 +5,37 @@ import {
   QueryDocumentSnapshot,
   serverTimestamp,
   SnapshotOptions,
+  Timestamp,
 } from 'firebase/firestore';
 
 import { db } from './../firebase';
 
 import type { CreatorDocData } from '#types/firestore/creator';
 
-export const creatorConverter: FirestoreDataConverter<Partial<CreatorDocData>> =
-  {
-    fromFirestore: (snapshot: QueryDocumentSnapshot, opts: SnapshotOptions) => {
-      return snapshot.data(opts) as CreatorDocData;
-    },
-    toFirestore: ({
+export const creatorConverter: FirestoreDataConverter<CreatorDocData> = {
+  fromFirestore: (snapshot: QueryDocumentSnapshot, opts: SnapshotOptions) => {
+    const data = snapshot.data(opts);
+    data.updatedAt = (data.updatedAt as Timestamp).toDate();
+    return data as CreatorDocData;
+  },
+  toFirestore: ({
+    creatorAddress,
+    creatorName,
+    description,
+    pinningPostId,
+  }) => {
+    if (!creatorAddress || !creatorName || !description || !pinningPostId) {
+      throw Error('Required values are not specified.');
+    }
+    return {
       creatorAddress,
       creatorName,
       description,
       pinningPostId,
-    }) => {
-      if (!creatorAddress || !creatorName || !description || !pinningPostId) {
-        throw Error('Required values are not specified.');
-      }
-      return {
-        createdAt: serverTimestamp(),
-        creatorAddress,
-        creatorName,
-        description,
-        pinningPostId,
-      };
-    },
-  };
+      updatedAt: serverTimestamp(),
+    };
+  },
+};
 
 export const getCreatorsCollectionRef = () =>
   collection(db, 'creators').withConverter(creatorConverter);
