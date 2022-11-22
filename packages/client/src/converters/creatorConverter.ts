1114c78d56ca5f7bd227fdc6dcde82385156a005
Haruki <nazawaharuki@gmail.com>
2022-11-22 12:12:43 +0900

    rename creatorConverter

--- packages/client/src/converters/creatorConverter.ts
+++ packages/client/src/converters/creatorConverter.ts
@@ -8,7 +8,7 @@ import {
   Timestamp,
 } from 'firebase/firestore';
 
-import { db } from './../firebase';
+import { db } from '../firebase';
 
 import type { CreatorDocData } from '#types/firestore/creator';
 
