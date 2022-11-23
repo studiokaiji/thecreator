c5ea7d760ed690f02b7e547906ad64fee3418eb3
Haruki <nazawaharuki@gmail.com>
2022-11-23 19:40:44 +0900

    with flex

--- packages/client/src/components/standalone/MainLoading.tsx
+++ packages/client/src/components/standalone/MainLoading.tsx
@@ -5,16 +5,18 @@ import Typography from '@mui/material/Typography';
 export const MainLoading = () => (
   <Box
     sx={{
-      left: '50%',
-      position: 'absolute',
-      textAlign: 'center',
-      top: '50%',
-      transform: 'translate(-50%, -50%)',
+      alignItems: 'center',
+      display: 'flex',
+      height: '100vh',
+      justifyContent: 'center',
+      width: '100%',
     }}
   >
-    <CircularProgress sx={{ left: '50% ', transform: 'translate(-50%)' }} />
-    <Typography fontWeight={500} mt={0.5}>
-      Loading
-    </Typography>
+    <Box>
+      <CircularProgress sx={{ left: '50% ', transform: 'translate(-50%)' }} />
+      <Typography fontWeight={500} mt={0.5}>
+        Loading
+      </Typography>
+    </Box>
   </Box>
 );
