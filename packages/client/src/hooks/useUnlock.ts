1e182034e7c30022462a705dc6e0d1a06dc2d7c3
Haruki <nazawaharuki@gmail.com>
2022-12-02 16:57:05 +0900

    add useUnlock

---
+++ packages/client/src/hooks/useUnlock.ts
@@ -0,0 +1,67 @@
+import { PublicLockV11, UnlockV11 } from '@unlock-protocol/contracts';
+import { BigNumberish, constants, Contract, providers, utils } from 'ethers';
+
+import { useContract } from './useContract';
+import { useWallet } from './useWallet';
+
+type CreateLockReq = {
+  lockCreator?: string;
+  baseToken?: string;
+  expirationDurationSeconds?: BigNumberish;
+  maxNumberOfKeys?: BigNumberish;
+  price: BigNumberish;
+  lockName: string;
+};
+
+type CreateLockOpts = {
+  request: CreateLockReq;
+  onCreateLockTxSend?: (response: providers.TransactionResponse) => void;
+  onCreateLockEnded?: (receipt: providers.TransactionReceipt) => void;
+};
+
+const VERSION = 11;
+
+export const useUnlock = (address: string) => {
+  const unlock = useContract(address, UnlockV11.abi);
+
+  const { account, library } = useWallet();
+
+  const createLock = async ({
+    onCreateLockEnded,
+    onCreateLockTxSend,
+    request: {
+      baseToken = constants.AddressZero,
+      expirationDurationSeconds = 30 * 60 * 60 * 24,
+      lockCreator = account,
+      lockName,
+      maxNumberOfKeys = constants.MaxUint256,
+      price,
+    },
+  }: CreateLockOpts) => {
+    const iface = new utils.Interface(PublicLockV11.abi);
+    const params = iface.encodeFunctionData(
+      'initialize(address,uint256,address,uint256,uint256,string)',
+      [
+        lockCreator,
+        expirationDurationSeconds,
+        baseToken,
+        price,
+        maxNumberOfKeys,
+        lockName,
+      ]
+    );
+
+    const tx = unlock.createUpgradeableLockAtVersion(params, VERSION);
+    onCreateLockTxSend && onCreateLockTxSend(tx);
+
+    const receipt = await tx.wait();
+    onCreateLockEnded && onCreateLockEnded(receipt);
+
+    const lockAddress = receipt.logs[0].address;
+    const lock = new Contract(lockAddress, PublicLockV11.abi, library);
+
+    return lock;
+  };
+
+  return { contract: unlock, createLock };
+};
