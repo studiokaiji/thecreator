22e45975716f18c600cb77d997a2fbe12123f03e
Haruki <nazawaharuki@gmail.com>
2022-12-08 13:28:38 +0900

    remove fade

--- packages/client/src/components/helpers/CenterModal.tsx
+++ packages/client/src/components/helpers/CenterModal.tsx
@@ -1,5 +1,4 @@
 import Box from '@mui/material/Box';
-import Fade from '@mui/material/Fade';
 import Modal, { ModalProps } from '@mui/material/Modal';
 
 type CenterModalProps = ModalProps & {
@@ -11,25 +10,23 @@ type CenterModalProps = ModalProps & {
 
 export const CenterModal = (props: CenterModalProps) => (
   <Modal {...props}>
-    <Fade in={props.open}>
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          borderRadius: 1,
-          boxShadow: 12,
-          height: props.height,
-          left: '50%',
-          maxHeight: props.maxHeight,
-          maxWidth: props.maxWidth,
-          p: 4,
-          position: 'absolute' as const,
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: props.width || 640,
-        }}
-      >
-        {props.children}
-      </Box>
-    </Fade>
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        borderRadius: 1,
+        boxShadow: 12,
+        height: props.height,
+        left: '50%',
+        maxHeight: props.maxHeight,
+        maxWidth: props.maxWidth,
+        p: 4,
+        position: 'absolute' as const,
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: props.width || 640,
+      }}
+    >
+      {props.children}
+    </Box>
   </Modal>
 );
