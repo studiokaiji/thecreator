1ec176aa3bf33bd5e1340c7d8df56a2fd6955336
Haruki <nazawaharuki@gmail.com>
2022-12-10 07:25:16 +0900

    add useOnlyAuthenticated

--- packages/client/src/hooks/useOnlyAuthenticated.tsx
+++ packages/client/src/hooks/useOnlyAuthenticated.tsx
@@ -2,9 +2,14 @@ import { useNavigate } from 'react-router-dom';
 
 import { useCurrentUser } from './useCurrentUser';
 
+import { MainLoading } from '@/components/standalone/MainLoading';
+
 export const useOnlyCurrentUser = () => {
   const navigate = useNavigate();
   const { checking, currentUser } = useCurrentUser();
+  if (checking) {
+    return <MainLoading />;
+  }
   if (!checking && !currentUser) {
     navigate(-1);
   }
