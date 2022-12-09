7e174295e83d233c99947d5544e59e49dc79ea27
Haruki <nazawaharuki@gmail.com>
2022-12-09 18:19:22 +0900

    add subscribe page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -7,6 +7,7 @@ import '@/firebase';
 import { SnackbarProvider } from './contexts/SnackbarContext';
 import { NotFound } from './pages/404';
 import { CreatorPage } from './pages/u/[id]';
+import { SubscribePage } from './pages/u/[id]/plans/[contractAddress]/subscribe';
 
 import { NavLayout } from '@/components/layout/NavLayout';
 import { AuthProvider } from '@/contexts/AuthContext';
@@ -41,7 +42,17 @@ function App() {
                     path="/edit/profile"
                   />
                   <Route element={<SupportersPage />} path="/edit/supporters" />
-                  <Route element={<CreatorPage />} path="/c/*" />
+                  <Route path="c">
+                    <Route path=":id">
+                      <Route index element={<CreatorPage />} />
+                      <Route path="subscribe">
+                        <Route
+                          element={<SubscribePage />}
+                          path=":lockAddress"
+                        />
+                      </Route>
+                    </Route>
+                  </Route>
                   <Route element={<NotFound />} path="/*" />
                 </Routes>
               </NavLayout>
