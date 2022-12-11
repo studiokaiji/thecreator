bf373ef7a8ce411815c45738277a58bd7383eb8c
Haruki <nazawaharuki@gmail.com>
2022-12-12 06:17:11 +0900

    remove network settings

--- packages/client/src/rpc-provider.ts
+++ packages/client/src/rpc-provider.ts
@@ -1,6 +1,5 @@
 import { providers } from 'ethers';
 
 export const rpcProvider = new providers.JsonRpcProvider(
-  import.meta.env.VITE_RPC_ENDPOINT_URL,
-  Number(import.meta.env.VITE_CHAIN_ID)
+  import.meta.env.VITE_RPC_ENDPOINT_URL
 );
