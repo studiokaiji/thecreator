c6b5f167c1c1e3944c2a51969fcd8903a7674d4c
Haruki <nazawaharuki@gmail.com>
2022-12-14 04:55:46 +0900

    signerOrAddress to signerOrProvider

--- packages/client/src/utils/multicall.ts
+++ packages/client/src/utils/multicall.ts
@@ -1,4 +1,4 @@
-import { Contract, providers } from 'ethers';
+import { Contract, providers, Signer } from 'ethers';
 
 import { MULTICALL } from '@/abis';
 
@@ -6,12 +6,12 @@ let contract: Contract;
 
 export const aggregate = (
   params: unknown[],
-  signerOrAddress?: providers.Provider
+  signerOrProvider?: providers.Provider | Signer
 ) => {
   contract = new Contract(
     import.meta.env.VITE_MULTICALL_ADDRESS,
     MULTICALL,
-    signerOrAddress
+    signerOrProvider
   );
   return contract.callStatic.aggregate(params);
 };
