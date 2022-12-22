022415a785feb575bfb0bb6a417b47373dc43b86
Haruki <nazawaharuki@gmail.com>
2022-12-22 15:22:00 +0900

    add updateUserEmail

--- packages/client/src/hooks/useUser.ts
+++ packages/client/src/hooks/useUser.ts
@@ -1,8 +1,10 @@
-import { deleteUser, updateProfile, User } from 'firebase/auth';
+import { deleteUser, updateEmail, updateProfile, User } from 'firebase/auth';
 import { getDoc, updateDoc } from 'firebase/firestore';
 import useSWR from 'swr';
 
+import { useAuth } from './useAuth';
 import { useCurrentUser } from './useCurrentUser';
+import { useWallet } from './useWallet';
 
 import type { UserDocData } from '#types/firestore/user';
 import { getUserDocRef } from '@/converters/userConverter';
@@ -11,6 +13,10 @@ const CURRENT_USER_DOES_NOT_EXIST_ERROR = Error('currentUser does not exist');
 
 export const useUser = () => {
   const { currentUser } = useCurrentUser();
+
+  const { authentication, getConnector } = useAuth();
+
+  const { activate } = useWallet();
 
   const handler = async (user: User) => {
     if (!user.uid) return null;
@@ -43,6 +49,25 @@ export const useUser = () => {
     await updateDoc(userRef, data);
   };
 
+  const updateUserEmail = async (email: string) => {
+    if (!currentUser) {
+      throw CURRENT_USER_DOES_NOT_EXIST_ERROR;
+    }
+
+    const connector = getConnector();
+    if (!connector) {
+      throw Error('Invalid connector');
+    }
+
+    await activate(connector, undefined, true);
+
+    const { user } = await authentication();
+    if (!user) {
+      throw Error('No authenticated');
+    }
+    await updateEmail(user, email);
+  };
+
   const removeUser = async () => {
     if (!currentUser) {
       throw CURRENT_USER_DOES_NOT_EXIST_ERROR;
@@ -52,5 +77,11 @@ export const useUser = () => {
 
   const swr = useSWR(currentUser, handler, { revalidateOnFocus: false });
 
-  return { removeUser, updateUserProfile, updateUserSettings, ...swr };
+  return {
+    removeUser,
+    updateUserEmail,
+    updateUserProfile,
+    updateUserSettings,
+    ...swr,
+  };
 };
