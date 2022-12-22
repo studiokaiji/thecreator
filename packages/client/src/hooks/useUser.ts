cc2e526a1e017c9578854a0f708e8c0b334136df
Haruki <nazawaharuki@gmail.com>
2022-12-22 23:49:37 +0900

    move updateUserEmail and removeUser to useCurrentUser

--- packages/client/src/hooks/useUser.ts
+++ packages/client/src/hooks/useUser.ts
@@ -1,10 +1,8 @@
-import { deleteUser, updateEmail, updateProfile, User } from 'firebase/auth';
+import { updateProfile, User } from 'firebase/auth';
 import { getDoc, updateDoc } from 'firebase/firestore';
 import useSWR from 'swr';
 
-import { useAuth } from './useAuth';
 import { useCurrentUser } from './useCurrentUser';
-import { useWallet } from './useWallet';
 
 import type { UserDocData } from '#types/firestore/user';
 import { getUserDocRef } from '@/converters/userConverter';
@@ -12,11 +10,7 @@ import { getUserDocRef } from '@/converters/userConverter';
 const CURRENT_USER_DOES_NOT_EXIST_ERROR = Error('currentUser does not exist');
 
 export const useUser = () => {
-  const { currentUser } = useCurrentUser();
-
-  const { authentication, getConnector } = useAuth();
-
-  const { activate } = useWallet();
+  const { currentUser, removeUser, updateUserEmail } = useCurrentUser();
 
   const handler = async (user: User) => {
     if (!user.uid) return null;
@@ -47,32 +41,6 @@ export const useUser = () => {
     }
     const userRef = getUserDocRef(currentUser.uid);
     await updateDoc(userRef, data);
-  };
-
-  const updateUserEmail = async (email: string) => {
-    if (!currentUser) {
-      throw CURRENT_USER_DOES_NOT_EXIST_ERROR;
-    }
-
-    const connector = getConnector();
-    if (!connector) {
-      throw Error('Invalid connector');
-    }
-
-    await activate(connector, undefined, true);
-
-    const { user } = await authentication();
-    if (!user) {
-      throw Error('No authenticated');
-    }
-    await updateEmail(user, email);
-  };
-
-  const removeUser = async () => {
-    if (!currentUser) {
-      throw CURRENT_USER_DOES_NOT_EXIST_ERROR;
-    }
-    await deleteUser(currentUser);
   };
 
   const swr = useSWR(currentUser, handler, { revalidateOnFocus: false });
