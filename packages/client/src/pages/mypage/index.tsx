a6891d4decc045c955fcfc3faa67f2f6c95ae89a
Haruki <nazawaharuki@gmail.com>
2022-12-25 03:36:59 +0900

    flex MenuCard

--- packages/client/src/pages/mypage/index.tsx
+++ packages/client/src/pages/mypage/index.tsx
@@ -5,15 +5,17 @@ import { useTranslation } from 'react-i18next';
 import { MainSpacingLayout } from '@/components/layout/MainSpacingLayout';
 import { MainCard } from '@/components/pages/mypage/MainCard';
 import { MenuCard } from '@/components/pages/mypage/MenuCard';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 export const MyPage = () => {
   const { t } = useTranslation();
+  const { width } = useWindowSize();
   return (
     <MainSpacingLayout>
       <Stack spacing={6}>
         <Typography variant="h1">{t('mypage')}</Typography>
         <Stack alignItems="flex-start" direction="row" gap={3}>
-          <MenuCard />
+          {width > 768 && <MenuCard />}
           <MainCard />
         </Stack>
       </Stack>
