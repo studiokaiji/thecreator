b2f5f9c3a648808f19526806cb5dac9db5e8ba10
Haruki <nazawaharuki@gmail.com>
2022-12-25 16:42:23 +0900

    remove Sections

--- packages/client/src/components/standalone/Creator/index.tsx
+++ packages/client/src/components/standalone/Creator/index.tsx
@@ -7,7 +7,6 @@ import { MainLoading } from '../MainLoading';
 
 import { ActionButtons } from './ActionButtons';
 import { ProfileImages } from './ProfileImages';
-import { Sections } from './Sections';
 import { Plans } from './plans';
 import { Posts } from './posts';
 
@@ -65,29 +64,15 @@ export const Creator = ({
             <Typography>{data.description}</Typography>
           </Stack>
         </Stack>
-        <Box sx={{ mx: 'auto', width: '100%' }}>
-          <Box sx={{ mx: 4 }}>
-            <Sections
-              sections={[
-                {
-                  component: (
-                    <Box sx={{ maxWidth: 640, mx: 'auto' }}>
-                      <Posts editable={editable} id={data.id || ''} />
-                    </Box>
-                  ),
-                  i18nKey: 'posts',
-                },
-                {
-                  component: (
-                    <Box sx={{ maxWidth: 1080, mx: 'auto' }}>
-                      <Plans creatorId={data.id} editable={editable} />
-                    </Box>
-                  ),
-                  i18nKey: 'plans',
-                },
-              ]}
-            />
-          </Box>
+        <Box sx={{ mx: 'auto' }}>
+          <Stack gap={8} sx={{ mx: 4 }}>
+            <Box sx={{ maxWidth: 1080, mx: 'auto', width: '100%' }}>
+              <Plans creatorId={data.id} editable={editable} />
+            </Box>
+            <Box sx={{ maxWidth: 640, mx: 'auto', width: '100%' }}>
+              <Posts editable={editable} id={data.id || ''} />
+            </Box>
+          </Stack>
         </Box>
       </Stack>
     </Stack>
