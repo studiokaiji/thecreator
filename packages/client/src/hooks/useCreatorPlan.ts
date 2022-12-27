d1465a582b3885196d7d090dbfba19d1f3e3cd68
Haruki <nazawaharuki@gmail.com>
2022-12-28 02:43:55 +0900

    fix args bug and add currentUser settings

--- packages/client/src/hooks/useCreatorPlan.ts
+++ packages/client/src/hooks/useCreatorPlan.ts
@@ -1,13 +1,19 @@
 import { getDoc } from 'firebase/firestore';
 import useSWR from 'swr';
 
+import { useCurrentUser } from './useCurrentUser';
+
 import { getCreatorPlanDocRef } from '@/converters/creatorPlanConverter';
 import { getPlansFromChain } from '@/utils/get-plans-from-chain';
 
 export const useCreatorPlan = (creatorId: string, planId: string) => {
   const docRef = getCreatorPlanDocRef(creatorId, planId);
 
-  const fetcher = async () => {
+  const { checking, currentUser } = useCurrentUser();
+
+  const fetcher = async (_: string, isChecking: boolean, uid: string) => {
+    if (isChecking) return;
+
     const snapshot = await getDoc(docRef);
 
     const docPlan = snapshot.data();
@@ -15,11 +21,11 @@ export const useCreatorPlan = (creatorId: string, planId: string) => {
       throw Error('Plan does not exist.');
     }
 
-    const plan = (await getPlansFromChain([docPlan], true))[0];
+    const plan = (await getPlansFromChain([docPlan], uid))[0];
     return plan;
   };
 
-  return useSWR(docRef.path, fetcher, {
+  return useSWR([docRef.path, checking, currentUser?.uid], fetcher, {
     revalidateOnFocus: false,
   });
 };
