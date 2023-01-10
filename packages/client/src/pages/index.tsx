72a37db11b03124fe0607d72ccee6915ecd567b8
Haruki <nazawaharuki@gmail.com>
2023-01-10 15:31:12 +0900

    add layout

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-
-import { useCurrentUser } from '@/hooks/useCurrentUser';
-
 export const IndexPage = () => {
-  const { currentUser } = useCurrentUser();
-
-  const [res, setRes] = useState('');
-
-  const onClickEchoIdTokenButtonHandler = async () => {
-    if (!currentUser) return;
-    const idToken = await currentUser.getIdToken();
-    fetch('http://127.0.0.1:8787/echo-id-token', {
-      headers: {
-        Authorization: idToken,
-      },
-      method: 'GET',
-    })
-      .then(async (r) => {
-        if (!r.ok) alert(r.status);
-        const data = await r.json();
-        setRes(JSON.stringify(data, null, 2));
-      })
-      .catch((e) => alert(String(e)));
-  };
-
   return (
     <div>
-      <pre>Res: {res}</pre>
-      <button onClick={onClickEchoIdTokenButtonHandler}>ECHO ID TOKEN</button>
+      <div>Hero Message and Image</div>
+      <div>Horizontal display of Creators</div>
+      <div>Features</div>
+      <div>Getting start</div>
     </div>
   );
 };
