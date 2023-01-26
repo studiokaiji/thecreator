9f806d19d3f8e2f5634f5462c56e4fed592ef853
Haruki <nazawaharuki@gmail.com>
2023-01-26 16:19:09 +0900

    to Croppable

--- packages/client/src/components/helpers/ImageCropper.tsx
+++ packages/client/src/components/helpers/ImageCropper.tsx
@@ -1,18 +1,24 @@
-import type { DialogProps } from '@mui/material/Dialog';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
 import { useCallback, useState } from 'react';
 import Cropper from 'react-easy-crop';
 import type { Area } from 'react-easy-crop';
-
-import { CenterModalWithTitle } from './CenterModalWithTitle';
+import { useTranslation } from 'react-i18next';
 
 import { useImage, UseImageData } from '@/hooks/useImage';
 
 type ImageCropperProps = {
   image: UseImageData;
   aspect: number;
-  imageType: Omit<ContentsType, 'audio' | 'text'>;
   onCropped: (image: UseImageData) => void;
-} & DialogProps;
+  onCancel: () => void;
+  cropContainerSize?: {
+    width: number;
+    height: number;
+  };
+};
 
 const getImage = (src: string) =>
   new Promise<HTMLImageElement>((resolve, reject) => {
@@ -68,43 +74,83 @@ const getCroppedImageBlob = async (src: string, pixelCrop: Area) => {
 
 export const ImageCropper = ({
   aspect,
+  cropContainerSize = {
+    height: 600,
+    width: 600,
+  },
   image,
-  imageType,
+  onCancel,
   onCropped,
 }: ImageCropperProps) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+
+  const { t } = useTranslation();
 
   const { createImage } = useImage();
 
-  const onCropComplete = useCallback(
-    async (_: Area, croppedAreaPixels: Area) => {
-      if (!croppedAreaPixels) return;
-      try {
-        const croppedImage = (await getCroppedImageBlob(
-          image.url,
-          croppedAreaPixels
-        )) as File;
-        const cropped = createImage(croppedImage, imageType);
-        onCropped(cropped);
-      } catch (e) {
-        console.error(e);
-      }
+  const onCropCompleteHandler = useCallback(
+    (_: Area, croppedAreaPixels: Area) => {
+      setCroppedAreaPixels(croppedAreaPixels);
     },
     []
   );
 
+  const onClickOkButtonHandler = async () => {
+    try {
+      if (!croppedAreaPixels) return;
+      const croppedImage = (await getCroppedImageBlob(
+        image.url,
+        croppedAreaPixels
+      )) as File;
+      const cropped = createImage(croppedImage, image.type);
+      onCropped(cropped);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <CenterModalWithTitle title="Crop">
-      <Cropper
-        aspect={aspect}
-        crop={crop}
-        image={image.url}
-        onCropChange={setCrop}
-        onCropComplete={onCropComplete}
-        onZoomChange={setZoom}
-        zoom={zoom}
-      />
-    </CenterModalWithTitle>
+    <Stack>
+      <Box
+        sx={{
+          height: cropContainerSize.height,
+          position: 'relative',
+          width: cropContainerSize.width,
+        }}
+      >
+        <Cropper
+          aspect={aspect}
+          crop={crop}
+          image={image.url}
+          onCropChange={setCrop}
+          onCropComplete={onCropCompleteHandler}
+          onMediaLoaded={(mediaSize) => {
+            setZoom(600 / mediaSize.naturalHeight);
+          }}
+          onZoomChange={setZoom}
+          zoom={zoom}
+        />
+      </Box>
+      <Stack pb={3} pt={4} px={4} spacing={2}>
+        <Slider
+          defaultValue={0}
+          max={5}
+          min={1}
+          onChange={(_, v) => setZoom(Number(v))}
+          step={0.05}
+          value={zoom}
+        />
+        <Stack direction="row" gap={1} justifyContent="right">
+          <Button onClick={onCancel} variant="outlined">
+            {t('cancel')}
+          </Button>
+          <Button onClick={onClickOkButtonHandler} variant="contained">
+            {t('ok')}
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
