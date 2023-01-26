420e1847e2b685d5db47a33af2874a0ffe61180e
Haruki <nazawaharuki@gmail.com>
2023-01-26 14:30:52 +0900

    add DialogTitle

--- packages/client/src/components/helpers/CenterModalWithTitle.tsx
+++ packages/client/src/components/helpers/CenterModalWithTitle.tsx
@@ -1,5 +1,4 @@
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import { CenterModal, CenterModalProps } from './CenterModal';
 
@@ -8,10 +7,7 @@ export type CenterModalWithTitleProps = CenterModalProps & { title: string };
 export const CenterModalWithTitle = (props: CenterModalWithTitleProps) => {
   return (
     <CenterModal {...props}>
-      <Stack spacing={3}>
-        <Typography variant="h4">{props.title}</Typography>
-        {props.children}
-      </Stack>
+      <DialogTitle>{props.title}</DialogTitle>
     </CenterModal>
   );
 };
