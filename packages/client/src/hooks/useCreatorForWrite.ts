6ed3581e079be35790d7c3f1f2c54f05b859a314
Haruki <nazawaharuki@gmail.com>
2023-01-26 17:09:25 +0900

    images uploadable

--- packages/client/src/hooks/useCreatorForWrite.ts
+++ packages/client/src/hooks/useCreatorForWrite.ts
@@ -1,12 +1,16 @@
 import { setDoc, updateDoc } from 'firebase/firestore';
 import { useMemo } from 'react';
 
+import { UseImageData } from './useImage';
 import { setIsCreatorFlag } from './useIsCreator';
+import { useUploadContents } from './useUploadContents';
 import { useWallet } from './useWallet';
 
 import { getCreatorDocRef } from '@/converters/creatorConverter';
 
 const refErr = Error('Creator document reference does not exist.');
+
+export type CreatorImageData = { icon?: UseImageData; header?: UseImageData };
 
 export const useCreatorForWrite = () => {
   const { account } = useWallet();
@@ -16,8 +20,13 @@ export const useCreatorForWrite = () => {
     return getCreatorDocRef(account);
   }, [account]);
 
-  const addCreator = async (creatorName: string, description: string) => {
+  const addCreator = async (
+    creatorName: string,
+    description: string,
+    images?: CreatorImageData
+  ) => {
     if (!docRef || !account) throw refErr;
+
     await setDoc(docRef, {
       createdAt: new Date(),
       creatorAddress: account,
@@ -32,25 +41,67 @@ export const useCreatorForWrite = () => {
       },
       updatedAt: new Date(),
     });
+
     setIsCreatorFlag(account, true);
+
+    if (images) {
+      await uploadImages(images);
+    }
   };
 
-  const updateCreator = async ({
-    creatorName,
-    description,
-    pinningPostId,
-    settings,
-  }: Partial<
-    Omit<CreatorDocData, 'id' | 'creatorAddress' | 'updatedAt' | 'createdAt'>
-  >) => {
+  const updateCreator = async (
+    {
+      creatorName,
+      description,
+      pinningPostId,
+      settings,
+    }: Partial<
+      Omit<CreatorDocData, 'id' | 'creatorAddress' | 'updatedAt' | 'createdAt'>
+    >,
+    images?: CreatorImageData
+  ) => {
     if (!docRef || !account) throw refErr;
+
     await updateDoc(docRef, {
       creatorName,
       description,
       pinningPostId,
       settings,
     });
+
+    if (images) {
+      await uploadImages(images);
+    }
   };
 
-  return { addCreator, docRef, updateCreator };
+  const { upload } = useUploadContents();
+
+  const uploadImages = async ({ header, icon }: CreatorImageData) => {
+    const images = [
+      {
+        contents: header,
+        contentsType: 'headerImage' as const,
+      },
+      {
+        contents: icon,
+        contentsType: 'iconImage' as const,
+      },
+    ];
+
+    const promises = [];
+
+    for (const { contents, contentsType } of images) {
+      if (!contents) return;
+      promises.push(
+        upload({
+          contents,
+          contentsType,
+        })
+      );
+    }
+
+    await Promise.all(promises);
+  };
+
+  return { addCreator, docRef, updateCreator, uploadImages };
 };
