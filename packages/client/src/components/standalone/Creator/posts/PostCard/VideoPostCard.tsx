728723a04d0816c7ede113b90680567ca3361521
Haruki <nazawaharuki@gmail.com>
2023-02-04 05:46:35 +0900

    add lazy loader

--- packages/client/src/components/standalone/Creator/posts/PostCard/VideoPostCard.tsx
+++ packages/client/src/components/standalone/Creator/posts/PostCard/VideoPostCard.tsx
@@ -1,7 +1,10 @@
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CircularProgress from '@mui/material/CircularProgress';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { CreatedAtTypography } from './common/CreateAtTypography';
 import { ReadMore } from './common/ReadMore';
@@ -22,7 +25,7 @@ export const VideoPostCard = ({
   const content = contents[0];
 
   const [iframeUrl, setIframeUrl] = useState('');
-  
+
   useEffect(() => {
     if (!content.url) {
       return;
@@ -35,6 +38,7 @@ export const VideoPostCard = ({
 
   const iframeParam = useVideoIframeParam({
     height: 'auto',
+    loading: 'lazy',
     src: iframeUrl,
     style: {
       aspectRatio: 16 / 9,
@@ -44,9 +48,47 @@ export const VideoPostCard = ({
     width: '100%',
   });
 
+  const [isLoadingIframe, setIsLoadingIframe] = useState(true);
+
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  useEffect(() => {
+    if (!iframeRef.current) {
+      return;
+    }
+    const loadListener = () => {
+      setIsLoadingIframe(false);
+      console.log('loaded');
+    };
+    iframeRef.current.removeEventListener('load', loadListener);
+    iframeRef.current.addEventListener('load', loadListener);
+  }, [iframeRef.current]);
+
   return (
     <Card>
-      <Iframe {...iframeParam} />
+      <Box
+        style={{ aspectRatio: 16 / 9 }}
+        sx={{ position: 'relative', width: '100%' }}
+      >
+        <Iframe
+          ref={iframeRef}
+          {...iframeParam}
+          sx={{ left: 0, position: 'absolute', zIndex: 2 }}
+        />
+        {isLoadingIframe && (
+          <Stack
+            alignItems="center"
+            height="100%"
+            justifyContent="center"
+            sx={{
+              backgroundColor: 'lightgray',
+              zIndex: 1,
+            }}
+            width="100%"
+          >
+            <CircularProgress sx={{ color: 'gray' }} />
+          </Stack>
+        )}
+      </Box>
       <CardContent sx={{ textAlign: 'left' }}>
         <ReadMore href={to}>
           <MinimalLink to={to}>
