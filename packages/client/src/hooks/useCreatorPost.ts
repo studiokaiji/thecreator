00c2c86194d6ca37deb0fa16b80cc1af28520b6e
Haruki <nazawaharuki@gmail.com>
2023-02-17 09:59:04 +0900

    add useCreatorPost

---
+++ packages/client/src/hooks/useCreatorPost.ts
@@ -0,0 +1,40 @@
+import { getDoc } from 'firebase/firestore';
+import { httpsCallable } from 'firebase/functions';
+import useSWR from 'swr';
+
+import { getCreatorPostDocRef } from '@/converters/creatorPostConverter';
+import { functions } from '@/firebase';
+
+export const useCreatorPost = (creatorId: string, postId: string) => {
+  const ref = getCreatorPostDocRef(creatorId, postId);
+
+  const fetcher = async () => {
+    const docSnapshot = await getDoc(ref);
+    if (!docSnapshot.exists()) {
+      return null;
+    }
+
+    const data = docSnapshot.data();
+    const post = { ...data };
+
+    try {
+      const { data: downloadUrlsResponseData } = await httpsCallable<
+        unknown,
+        GetDownloadSignedUrlResponse
+      >(
+        functions,
+        'getDownloadSignedUrls'
+      )({ posts: [{ creatorId, postId }] });
+
+      downloadUrlsResponseData[0].urls.map((url, i) => {
+        post.contents[i].url = url;
+      });
+
+      return post;
+    } catch (e) {
+      return post;
+    }
+  };
+
+  return useSWR(`/c/${creatorId}/posts/${postId}`, fetcher);
+};
